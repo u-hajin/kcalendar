8f40e713c66e0d5e26ccafe5e4ac7b968929f979
YooSuYeon <usuyn000@gmail.com>
2022-05-06 02:04:05 +0900

    기본 UI (#4)
    * design: 디자인 리소스 파일 추가
    
    * build: svg 이미지 활용을 위한 종속 항목 추가
    
    * design: 캘린더 메인 화면 ui
    
    * design: 목표, 그래프 ui
    
    * design: 앱 색상 추가
    
    * design: 앱 색상 적용
    
    * design: 섭취 음식 정보 입력 ui
    
    * design: 권장 칼로리 계산 ui
    
    * design: 슬라이딩으로 탭 전환 ui
    
    * feature: 캘린더, 목표 및 그래프 레이아웃 전환

--- app/src/main/java/com/kkwakjavacoding/kcalendar/MainActivity.kt
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/MainActivity.kt
@@ -2,10 +2,28 @@ package com.kkwakjavacoding.kcalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.kkwakjavacoding.kcalendar.adapter.MyViewPageAdapter
+import com.kkwakjavacoding.kcalendar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    val tabTexts = arrayListOf<String>("kcalendar", "graph & goal")
+    val tabIcons = arrayListOf<Int>(R.drawable.ic_date_range_fill, R.drawable.ic_graph)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initLayout()
+    }
+
+    private fun initLayout() {
+        binding.viewPager.adapter = MyViewPageAdapter(this)
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.setIcon(tabIcons[position])
+            tab.text = tabTexts[position]
+        }.attach()
     }
 }
