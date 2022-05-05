f0ef31f777a8d90479bdf8987fb8265396681d58
YooSuYeon <usuyn000@gmail.com>
2022-05-06 01:20:27 +0900

    feature(firebase): 섭취 음식 삽입 및 조회 기능

--- app/src/main/java/com/kkwakjavacoding/kcalendar/firebase/Database.kt
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/firebase/Database.kt
@@ -4,6 +4,7 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.kkwakjavacoding.kcalendar.fooddatabase.Food
 import kotlinx.coroutines.tasks.asDeferred
 
 class Database {
@@ -13,6 +14,11 @@ class Database {
 
     fun insertGoal(date: String, nutrition: Nutrition) {
         database.child(id).child(date).child("goal").setValue(nutrition)
+    }
+
+    fun insertFood(date: String, time: String, food: Food) {
+        database.child(date).child(time).child(food.name)
+            .setValue(food) // userToken 밑 child 변경 필요.
     }
 
     suspend fun getGoal(date: String): HashMap<String, Any> {
@@ -30,6 +36,19 @@ class Database {
         }
 
         return goal
+    }
+
+    suspend fun getFoodName(date: String, time: String): ArrayList<String> {
+        val task: Task<DataSnapshot> = database.child(date).child(time).get()
+        val deferredDataSnapshot: kotlinx.coroutines.Deferred<DataSnapshot> = task.asDeferred()
+        val data: Iterable<DataSnapshot> = deferredDataSnapshot.await().children
+
+        var foods: ArrayList<String> = ArrayList()
+        while (data.iterator().hasNext()) {
+            foods.add(data.iterator().next().key!!)
+        }
+
+        return foods
     }
 
 }
