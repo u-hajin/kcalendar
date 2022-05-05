9d85783754167f00a6bc46af7b3164d3f76776c9
YooSuYeon <usuyn000@gmail.com>
2022-05-06 01:22:39 +0900

    feature(firebase): 섭취 음식 정보, 일일 목표 class 추가

---
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/firebase/Nutrition.kt
@@ -0,0 +1,10 @@
+package com.kkwakjavacoding.kcalendar.firebase
+
+data class Nutrition(
+    var kcal: Double,
+    var carbs: Double,
+    var protein: Double,
+    var fat: Double,
+    var sugars: Double,
+    var sodium: Double
+)
