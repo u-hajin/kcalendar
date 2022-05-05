a52502673f681cce4566042551afa23f7a07e306
YooSuYeon <usuyn000@gmail.com>
2022-05-06 01:09:31 +0900

    feature: 음식 정보 내부 db 기능 추가

---
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/fooddatabase/Food.kt
@@ -0,0 +1,23 @@
+package com.kkwakjavacoding.kcalendar.fooddatabase
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.google.firebase.database.IgnoreExtraProperties
+
+@IgnoreExtraProperties
+@Entity(tableName = "food_table")
+data class Food(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val location: String,
+    val classification: String,
+    val serving: Double,
+    val unit: String,
+    val kcal: Double,
+    val carbs: Double?,
+    val protein: Double?,
+    val fat: Double?,
+    val sugars: Double?,
+    val sodium: Double?,
+)
---
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/fooddatabase/FoodDao.kt
@@ -0,0 +1,25 @@
+package com.kkwakjavacoding.kcalendar.fooddatabase
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FoodDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun addFood(food: Food)
+
+    @Insert
+    fun addFoodDb(foods: List<Food>)
+
+    @Update
+    fun updateFood(food: Food)
+
+    @Delete
+    fun deleteFood(food: Food)
+
+    @Query("SELECT * FROM food_table")
+    fun readAllData(): Flow<List<Food>>
+
+    @Query("SELECT * FROM food_table WHERE name LIKE :searchQuery")
+    fun searchDatabase(searchQuery: String): Flow<List<Food>>
+}
