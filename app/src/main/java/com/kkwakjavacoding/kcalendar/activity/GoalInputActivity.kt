b4bc6597ee2724b04ecd1e557e95a12c0d363496
somin-jeong <81250561+somin-jeong@users.noreply.github.com>
2022-05-08 00:34:48 +0900

    사용자 목표 UI 누락 activity 추가 (#10)
    * design goalinputui
    
    * Acticity 추가

---
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/activity/GoalInputActivity.kt
@@ -0,0 +1,12 @@
+package com.kkwakjavacoding.kcalendar.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kkwakjavacoding.kcalendar.R
+
+class GoalInputActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_goal_input)
+    }
+}
