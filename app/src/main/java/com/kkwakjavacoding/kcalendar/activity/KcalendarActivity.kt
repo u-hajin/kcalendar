b9cf9c337f06fe6a0ee006557faa96722518292d
YooSuYeon <usuyn000@gmail.com>
2022-05-13 03:34:26 +0900

    feat: 날짜, 식사 시간 전달

--- app/src/main/java/com/kkwakjavacoding/kcalendar/activity/KcalendarActivity.kt
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/activity/KcalendarActivity.kt
@@ -5,18 +5,25 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.kkwakjavacoding.kcalendar.R
 import com.kkwakjavacoding.kcalendar.databinding.ActivityKcalendarBinding
 import org.tensorflow.lite.Interpreter
 import java.io.FileInputStream
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import java.nio.channels.FileChannel
+import java.text.SimpleDateFormat
+import java.util.*
 
 const val REQUEST_GALLERY = 100
 const val REQUEST_CAMERA = 200
 const val IMAGE_MEAN = 127.5f
 const val IMAGE_STD = 127.5f
+const val BREAKFAST = "breakfast"
+const val LUNCH = "lunch"
+const val DINNER = "dinner"
 
 class KcalendarActivity : AppCompatActivity() {
     private var inputBuffer: ByteBuffer? = null
@@ -32,6 +39,7 @@ class KcalendarActivity : AppCompatActivity() {
     private var month = 0
     private var day = 0
     private var date = ""
+    private var time = BREAKFAST
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,7 +48,14 @@ class KcalendarActivity : AppCompatActivity() {
 
         interpreter = Interpreter(loadModel(), null)
 
+        binding.breakfastBtn.setOnClickListener(ButtonListener())
+        binding.lunchBtn.setOnClickListener(ButtonListener())
+        binding.dinnerBtn.setOnClickListener(ButtonListener())
+
         calendarListener()
+
+        val currentDate = Calendar.getInstance().time
+        date = SimpleDateFormat("yyyy-MM-dd").format(currentDate)
 
         binding.apply {
             foodAddBtn.setOnClickListener {
@@ -50,11 +65,37 @@ class KcalendarActivity : AppCompatActivity() {
 
     }
 
+    inner class ButtonListener : View.OnClickListener {
+        override fun onClick(v: View?) {
+            when (v?.id) {
+                binding.breakfastBtn.id -> {
+                    time = BREAKFAST
+                    binding.breakfastBtn.setBackgroundResource(R.drawable.left_round_selected)
+                    binding.lunchBtn.setBackgroundResource(R.color.pastel_green)
+                    binding.dinnerBtn.setBackgroundResource(R.drawable.right_round)
+                }
+                binding.lunchBtn.id -> {
+                    time = LUNCH
+                    binding.breakfastBtn.setBackgroundResource(R.drawable.left_round)
+                    binding.lunchBtn.setBackgroundResource(R.color.deep_green)
+                    binding.dinnerBtn.setBackgroundResource(R.drawable.right_round)
+                }
+                binding.dinnerBtn.id -> {
+                    time = DINNER
+                    binding.breakfastBtn.setBackgroundResource(R.drawable.left_round)
+                    binding.lunchBtn.setBackgroundResource(R.color.pastel_green)
+                    binding.dinnerBtn.setBackgroundResource(R.drawable.right_round_selected)
+                }
+            }
+        }
+    }
+
     private fun calendarListener() {
         binding.calendarView.setOnDateChangeListener { _, i, i2, i3 ->
             year = i
             month = i2 + 1
             day = i3
+            convertDateFormat()
         }
     }
 
@@ -77,6 +118,8 @@ class KcalendarActivity : AppCompatActivity() {
             predict()
             val intent2 = Intent(this, FoodCustomActivity::class.java)
             intent2.putExtra("result", predictResult)
+            intent2.putExtra("date", date)
+            intent2.putExtra("time", time)
             startActivity(intent2)
         } else if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CAMERA) {
             super.onActivityResult(requestCode, resultCode, data)
