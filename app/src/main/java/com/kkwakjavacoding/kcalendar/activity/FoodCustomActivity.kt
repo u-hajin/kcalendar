d0e91e8b63e12d2ccb865bd423166a052faea4fe
YooSuYeon <usuyn000@gmail.com>
2022-05-13 00:15:31 +0900

    feat: 음식 인식 후 정보 제공
    분류 체크박스 자동 체크
    체크박스 추가 체크 가능
    해당 분류 가진 brand를 조회해 brandSpinner에 제공
    brand의 음식들 중 체크된 분류를 가진 음식들만 foodNameSpinner에 제공
    foodNameSpinner에서 음식 선택 시 해당 음식 조회 후 정보 제공

--- app/src/main/java/com/kkwakjavacoding/kcalendar/activity/FoodCustomActivity.kt
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/activity/FoodCustomActivity.kt
@@ -3,38 +3,44 @@ package com.kkwakjavacoding.kcalendar.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.CheckBox
-import android.widget.Toast
+import android.widget.*
 import androidx.lifecycle.ViewModelProvider
 import com.kkwakjavacoding.kcalendar.databinding.ActivityFoodCustomBinding
+import com.kkwakjavacoding.kcalendar.firebase.Database
 import com.kkwakjavacoding.kcalendar.fooddatabase.Food
 import com.kkwakjavacoding.kcalendar.fooddatabase.FoodViewModel
 import com.kkwakjavacoding.kcalendar.weightdatabase.WeightViewModel
 import kotlin.math.roundToInt
 
-const val IMAGE_MEAN = 127.5f
-const val IMAGE_STD = 127.5f
-
 class FoodCustomActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityFoodCustomBinding
-    lateinit var foodViewModel: FoodViewModel
-    lateinit var weightViewModel: WeightViewModel
-    lateinit var brandList: ArrayList<String>
-    lateinit var foodNameList: ArrayList<String>
-    lateinit var checkBoxList: ArrayList<CheckBox>
-    var brand: String = ""
-    lateinit var predictResult: String
+    private lateinit var binding: ActivityFoodCustomBinding
+    private lateinit var predictResult: String
+    private lateinit var foodViewModel: FoodViewModel
+    private lateinit var weightViewModel: WeightViewModel
+    private lateinit var checkBoxList: ArrayList<CheckBox>
+    private lateinit var food: Food
+    private var brand: String = ""
+    private var brandList: ArrayList<String> = ArrayList()
+    private var foodNameList: ArrayList<String> = ArrayList()
+    private lateinit var brandAdapter: ArrayAdapter<String>
+    private lateinit var foodNameAdapter: ArrayAdapter<String>
+    private var selected: String = ""
+    private var classList: ArrayList<String> = ArrayList()
+    private var searchList: List<Food> = listOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityFoodCustomBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        predictResult = intent.getStringExtra("result")!!
         getCheckBox()
+
+        // date, time도 intent로 받아야 함.
+        predictResult = intent.getStringExtra("result")!!
+
+        setBrandSpinnerListener()
+        setFoodNameSpinnerListener()
+        buttonListener()
 
         foodViewModel = ViewModelProvider(
             this,
@@ -46,7 +52,12 @@ class FoodCustomActivity : AppCompatActivity() {
             WeightViewModel.Factory(application)
         )[WeightViewModel::class.java]
 
-        getFoodInfo()
+        foodNameList.add("")
+        foodNameAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, foodNameList)
+        binding.foodNameSpinner.adapter = foodNameAdapter
+
+        showPredictResult()
     }
 
     private fun getCheckBox() {
@@ -62,11 +73,9 @@ class FoodCustomActivity : AppCompatActivity() {
             binding.stirCheckBox,
             binding.breadCheckBox,
             binding.sandwichCheckBox,
-            binding.dairyCheckBox,
             binding.drinkCheckBox,
             binding.pancakeCheckBox,
             binding.boiledCheckBox,
-            binding.stewCheckBox,
             binding.steamedCheckBox,
             binding.vegetableCheckBox,
             binding.chickenCheckBox,
@@ -76,103 +85,61 @@ class FoodCustomActivity : AppCompatActivity() {
         )
     }
 
-    private fun findFirstFood(it: List<Food>) { // 제일 맨 위 음식 가져오기
-        binding.foodName.text = it[0].name
-        binding.foodQuantity.text = it[0].serving.roundToInt().toString() + it[0].unit
-        binding.foodKcal.text = it[0].kcal.toString()
-        binding.foodCarbs.text = it[0].carbs.toString()
-        binding.foodProtein.text = it[0].protein.toString()
-        binding.foodFat.text = it[0].fat.toString()
-        binding.foodSugars.text = it[0].sugars.toString()
-        binding.foodSodium.text = it[0].sodium.toString()
-    }
-
-    private fun setCheckBox(it: List<Food>) { // spinner 채우기
-        var classification = it[0].classification
-
-        for (i in checkBoxList) {
-            if (classification == i.text.toString()) {
-                i.isChecked = true
+    private fun showPredictResult() { // 인식 결과를 음식 정보 db에서 찾아 화면에 제공
+        foodViewModel.searchName("%$predictResult%").observe(this) {
+            if (it.isEmpty()) {
+                Toast.makeText(this, "일치하는 음식이 없습니다.", Toast.LENGTH_LONG).show()
+            } else {
+                food = it[0].copy()
+                showFoodInfo()
+                setCheckBoxAuto()
+                setBrandSpinnerFirst(it)
             }
         }
     }
 
-    private fun setBrandSpinner(it: List<Food>) { // spinner 채우기
-        brandList = ArrayList()
+    private fun showFoodInfo() {
+        binding.foodName.text = food.name
+        binding.foodQuantity.text = food.serving.roundToInt().toString() + food.unit
+        binding.foodKcal.text = food.kcal.toString()
+        binding.foodCarbs.text = food.carbs.toString()
+        binding.foodProtein.text = food.protein.toString()
+        binding.foodFat.text = food.fat.toString()
+        binding.foodSugars.text = food.sugars.toString()
+        binding.foodSodium.text = food.sodium.toString()
+    }
 
+    private fun setCheckBoxAuto() { // 인식하고 체크박스 자동 체크
+        brand = food.brand
+
+        for (i in checkBoxList) {
+            if (food.classification == i.text.toString()) {
+                classList.add(food.classification)
+                i.isChecked = true
+                break
+            }
+        }
+    }
+
+    // 이름에 predictResult가 들어가고 상세분류가 같은 음식들의 브랜드로 spinner 채우기
+    private fun setBrandSpinnerFirst(it: List<Food>) {
         for (i in it) {
-            if (i.classification == it[0].classification && !brandList.contains(i.brand)) {
+            if (i.classification == food.classification && !brandList.contains(i.brand)) {
                 brandList.add(i.brand)
             }
         }
 
-        val adapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, brandList)
-
-        binding.brandSpinner.adapter = adapter
-
-        binding.brandSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                brand = binding.brandSpinner.selectedItem.toString()
-                getBrandFoodName(it)
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-            }
-        }
+        brandAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, brandList)
+        binding.brandSpinner.adapter = brandAdapter
+        setBrandSpinnerListener()
     }
 
-    private fun setFoodNameSpinner() {
-
-    }
-
-    private fun getFoodInfo() {
-        var searchQuery = "%$predictResult%"
-
-        foodViewModel.searchName(searchQuery).observe(this) {
-
-            if (it.isEmpty()) {
-                Toast.makeText(this, "일치하는 이름이 없습니다.", Toast.LENGTH_LONG).show()
-            } else {
-                findFirstFood(it)
-                setCheckBox(it)
-                setBrandSpinner(it)
-            }
-
-        }
-
-    }
-
-    private fun updateSpinner() {
-
-        for (i in checkBoxList) {
-            if (i.isChecked) {
-
-            }
-        }
-
-        // 제품명 업데이트도 같이 해야됨.
-    }
-
-    private fun getBrandFoodName(it: List<Food>) {
-        foodNameList = ArrayList()
-
-        for (i in it) {
-
-            if (i.brand == this.brand) {
-                foodNameList.add(i.name)
-            }
-
-        }
-        val adapter =
-            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, foodNameList)
-
-        binding.foodNameSpinner.adapter = adapter
-
-        binding.foodNameSpinner.onItemSelectedListener =
+    private fun setBrandSpinnerListener() {
+        binding.brandSpinner.onItemSelectedListener =
             object : AdapterView.OnItemSelectedListener {
                 override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-
+                    brand = binding.brandSpinner.selectedItem.toString()
+                    setFoodNameSpinner()
                 }
 
                 override fun onNothingSelected(p0: AdapterView<*>?) {
@@ -180,5 +147,80 @@ class FoodCustomActivity : AppCompatActivity() {
             }
     }
 
+    private fun setFoodNameSpinnerListener() {
+        binding.foodNameSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    selected = binding.foodNameSpinner.selectedItem.toString()
+                    matchSelectedFood()
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                }
+            }
+    }
+
+    fun updateBrandSpinner(view: View) {
+        brandList.clear()
+        classList.clear()
+
+        for (i in checkBoxList) {
+            if (i.isChecked) {
+                classList.add(i.text.toString())
+                foodViewModel.searchClassification(i.text.toString()).observe(this) {
+                    for (j in it) {
+                        if (!brandList.contains(j.brand)) {
+                            brandList.add(j.brand)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setFoodNameSpinner() {
+        foodNameList.clear()
+
+        foodViewModel.searchBrand(brand).observe(this) {
+            for (i in it) {
+                if (classList.contains(i.classification)) {
+                    foodNameList.add(i.name)
+                }
+            }
+            searchList = it
+        }
+
+    }
+
+    private fun matchSelectedFood() {
+//        foodViewModel.searchBrand(brand).observe(this) {
+//            for (i in it) {
+//                if (i.name == selected) {
+//                    food = i
+//                    break
+//                }
+//            }
+//            showFoodInfo()
+//        }
+        for (i in searchList) {
+            if (i.name == selected) {
+                food = i
+                break
+            }
+        }
+        showFoodInfo()
+    }
+
+    private fun buttonListener() {
+        binding.customSaveBtn.setOnClickListener {
+            val db = Database()
+//            db.insertFood(intent.getStringExtra("date")!!, intent.getStringExtra("time")!!, food)
+            // 다시 kcalendarActivity로 화면 전환
+        }
+
+        binding.customCancelBtn.setOnClickListener {
+            finish()
+        }
+    }
 
 }
