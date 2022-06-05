ded4e45c941b2a576d2de1282f3a9393f888ab5a
GIRL-0 <63105233+GIRL-0@users.noreply.github.com>
2022-06-05 23:33:42 +0900

    intro 추가 (#50)
    * design: 그래프 잘림 문제 해결
    
    * design: 키 입력 text 가운데 맞춤
    
    * design: text 양 끝 간격 좁힘
    
    * design: 인트로 화면 추가

---
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/activity/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.kkwakjavacoding.kcalendar.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kkwakjavacoding.kcalendar.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
