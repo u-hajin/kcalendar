9199d029d8ba00dcb25f73116be0b6dc62881818
YooSuYeon <usuyn000@gmail.com>
2022-06-06 00:03:16 +0900

    feat: intro 화면 제공

--- app/src/main/java/com/kkwakjavacoding/kcalendar/activity/IntroActivity.kt
+++ app/src/main/java/com/kkwakjavacoding/kcalendar/activity/IntroActivity.kt
@@ -1,7 +1,9 @@
 package com.kkwakjavacoding.kcalendar.activity
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import com.kkwakjavacoding.kcalendar.R
 
@@ -9,5 +11,15 @@ class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            var intent = Intent(this, KcalendarActivity::class.java)
+            startActivity(intent)
+        }, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        finish()
     }
 }
